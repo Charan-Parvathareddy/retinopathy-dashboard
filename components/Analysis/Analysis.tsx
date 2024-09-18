5925d9a0b7ca4f1cee535ef649e12c89bce5350b
Charan-Parvathareddy <parvathareddy.charan@gmail.com>
2024-09-19 00:36:55 +0530

    47

--- components/Analysis/Analysis.tsx
+++ components/Analysis/Analysis.tsx
@@ -285,21 +285,31 @@ const SVGLineGlowAnimate: React.FC<SVGLineGlowAnimateProps> = ({
 const GlowingLineGrid = () => {
   return (
     <div className="absolute inset-0 z-20 pointer-events-none">
-      <div className="w-full h-full grid grid-cols-24 grid-rows-24">
-        {[...Array(8)].map((_, index) => (
+      <div className="w-full h-full grid grid-cols-64 grid-rows-8">
+        {[...Array(512)].map((_, index) => (
           <div key={index} className="flex items-center justify-center">
             <SVGLineGlowAnimate 
-              movementDelay={index * 500} 
+              movementDelay={index * 200} 
               id={index} 
-              additionalHeight={index % 4 === 1 || index % 4 === 2 ? 20 : 0} 
-              initialGradientY={index % 4 === 0 || index % 4 === 3 ? 20 : 0}
+              additionalHeight={
+                index % 64 === 1 || index % 64 === 2 || index % 64 === 5 || index % 64 === 6 ? 20 : 0
+              } 
+              initialGradientY={
+                index % 64 === 0 || index % 64 === 3 || index % 64 === 4 || index % 64 === 7 ? 20 : 0
+              }
             />
           </div>
         ))}
+
+        {/* Center horizontal line */}
+        <div className="col-span-64 row-start-4 row-end-5">
+          <div className="w-full h-[2px] bg-white opacity-70" />
+        </div>
       </div>
     </div>
   );
 };
+
 
 const DistortedGlass = () => {
   const [isVisible, setIsVisible] = useState(true);
