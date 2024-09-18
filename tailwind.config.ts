95da8b15b8cf9f95539b939d4592f1cfd1554542
Charan-Parvathareddy <parvathareddy.charan@gmail.com>
2024-09-19 01:24:00 +0530

    56

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,6 +80,17 @@ const config = {
         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
       keyframes: {
+        "shine-pulse": {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
